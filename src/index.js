afbf8bf12bbbf1260cc4bae35f2e9e986844a5fb
Felix Leenborg Anthonessen <felix.leenborg.anthonessen@communicate.no>
2022-10-19 20:56:54 +0200

    removed unnessesary import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import { BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: "https://resultatservice-api.stage-sumo.tv2.no/api/graphql",
@@ -14,11 +13,9 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
